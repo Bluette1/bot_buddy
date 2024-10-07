8271cefc62897db5ba34082888c43e40807357da
Bluette1 <marylene.sawyer@gmail.com>
2024-10-07 16:27:16 +0200

    Refactor Tests

--- tests/test_chatgpt_integration.py
+++
@@ -1,4 +0,0 @@
-async def test_chatgpt_integration(mocker):
-    mocker.patch('openai.OpenAI.chat.completions.create', return_value={'choices': [{'message': {'content': 'Response'}}]})
-    response = await chat_with_gpt("Hello")
-    assert response == "Response"
--- tests/test_conversation_history.py
+++
@@ -1,5 +0,0 @@
-async def test_conversation_history_storage(mocker):
-    mock_mongo_collection = mocker.Mock()
-    mocker.patch('pymongo.MongoClient', return_value=mock_mongo_collection)
-    await on_message(mock_message)
-    mock_mongo_collection.update_one.assert_called_with(...)
