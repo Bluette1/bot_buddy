075b6a19ef8aed9adc09a84e432bdcce10c16dc8
Bluette1 <marylene.sawyer@gmail.com>
2024-10-21 17:31:18 +0200

    feat: Scrape All The Pages on `Quotes To Scrape`
    - Scrape all pages on the site
    
    Issue #9

--- scraper.py
+++ scraper.py
@@ -2,21 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape_quotes():
-    url = "http://quotes.toscrape.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
     quotes = []
 
-    for quote in soup.find_all('div', class_='quote'):
-        text = quote.find('span', class_='text').get_text()
-        author = quote.find('small', class_='author').get_text()
-        tags = [tag.get_text() for tag in quote.find_all('a', class_='tag')]
-        quotes.append({
-            'text': text,
-            'author': author,
-            'tags': tags
-        })
+    for page in range(1, 11):  # Pages 1 to 10
+        url = f"http://quotes.toscrape.com/page/{page}/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        for quote in soup.find_all('div', class_='quote'):
+            text = quote.find('span', class_='text').get_text()
+            author = quote.find('small', class_='author').get_text()
+            tags = [tag.get_text() for tag in quote.find_all('a', class_='tag')]
+            quotes.append({
+                'text': text,
+                'author': author,
+                'tags': tags
+            })
 
     return quotes
 
