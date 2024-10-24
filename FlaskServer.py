c61dca2e1d133d155e48e2a291cc76e0bdcb651f
Bluette1 <marylene.sawyer@gmail.com>
2024-10-24 05:28:00 +0200

    refactor: Refactor FlaskServer
    - RRefactor FlaskServer for Testing
    
    Issue #13

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,6 +1,8 @@
+
 from flask import Flask, request, jsonify
 import os
 import discord
+
 
 class FlaskServer:
     def __init__(self, bot, guild_id, role_id, premier_channel_id):
@@ -19,26 +21,29 @@ class FlaskServer:
             amount = data.get('amount', '0')
             message = data.get('message', '')
 
-            async def process_donation():
-                guild = self.bot.get_guild(self.guild_id)
-                member = discord.utils.find(lambda m: m.name == donor_name, guild.members)
-
-                if member:
-                    role = discord.utils.get(guild.roles, name="Donor")
-
-                    if role is None:
-                        # Create the role if it doesn't exist
-                        role = await guild.create_role(name="Donor", permissions=discord.Permissions(send_messages=True))
-                    if role not in member.roles:
-                        await member.add_roles(role)
-                        print(f"Assigned role to {member.name}")
-
-                channel = self.bot.get_channel(self.premier_channel_id)
-                if channel:
-                    await channel.send(f"🎉 **{donor_name}** just donated **${amount}** on Ko-Fi! {message}")
-
-            self.bot.loop.create_task(process_donation())
+            # Call the process_donation method
+            self.bot.loop.create_task(
+                self.process_donation(donor_name, amount, message))
             return jsonify({'status': 'success'}), 200
+
+    async def process_donation(self, donor_name, amount, message):
+        guild = self.bot.get_guild(self.guild_id)
+        member = discord.utils.find(
+            lambda m: m.name == donor_name, guild.members)
+
+        if member:
+            role = discord.utils.get(guild.roles, name="Donor")
+
+            if role is None:
+                # Create the role if it doesn't exist
+                role = await guild.create_role(name="Donor", permissions=discord.Permissions(send_messages=True))
+            if role not in member.roles:
+                await member.add_roles(role)
+                print(f"Assigned role to {member.name}")
+
+        channel = self.bot.get_channel(self.premier_channel_id)
+        if channel:
+            await channel.send(f"🎉 **{donor_name}** just donated **${amount}** on Ko-Fi! {message}")
 
     def run(self):
         print("Starting Flask server...")
